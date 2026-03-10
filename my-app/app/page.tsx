41b6fe47a3287d9072d6a1a269c9add5f266d8fb
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2026-03-10 23:51:14 +0900

    indexをpagesに移動・layoutで繋げる

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,65 +1,37 @@
 import Image from "next/image";
+'use client';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div id="container">
+      <svg id="field" width="1240" height="827" className="play" version="1.1">
+        <image id="beach" href="/images/beach.png" x="220" y="140" width="800" height="450" />
+        <image id="obake" x="0" y="0" width="300" height="300" />
+        <image id="kani" href="/images/kani.png" x="250" y="450" width="60" height="40" />
+        <image id="suika" href="/images/suika1.png" x="250" y="450" width="80" />
+        <image id="theater" href="/images/theater.png" x="0" y="0" width="1240" height="827" className="closed" />
+        
+        <g id="audience" className="closed">
+          <g stroke="red" strokeWidth={8} className="psyllium">
+            <path d="M580,710 L600,670" />
+            <path d="M880,800 L900,760" />
+            <path d="M250,715 L270,675" />
+          </g>
+          <g stroke="blue" strokeWidth={8} className="psyllium">
+            <path d="M650,830 L670,790" />
+            <path d="M160,750 L140,710" />
+            <path d="M490,770 L470,730" />
+          </g>
+          <g stroke="yellow" strokeWidth={8} className="psyllium">
+            <path d="M410,765 L390,725" />
+            <path d="M980,730 L1000,690" />
+            <path d="M760,705 L740,665" />
+          </g>
+        </g>
+        
+        <g id="balloons"></g>
+        <g id="comments"></g>
+      </svg>
     </div>
   );
 }
