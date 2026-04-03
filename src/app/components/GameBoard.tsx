71ee04731553ff0f07d45e53c93bb50c1d831696
Jasmine <103996128+jasmine-tokudome@users.noreply.github.com>
2026-04-03 22:53:36 +0900

    外部から受け取るデータ（Props）の型を指定

--- src/app/components/GameBoard.tsx
+++ src/app/components/GameBoard.tsx
@@ -2,3 +2,11 @@
 'use client';
 
 import { GameObject, GameMode, CommentData, getObakePos, getKaniPos, getSuikaPos } from '@/lib/game/logic';
+
+interface Props {
+    obake: GameObject;
+    kani: GameObject;
+    suika: GameObject;
+    gameMode: GameMode;
+    comments: CommentData[];
+}
